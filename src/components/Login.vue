8bac83037efcbad13696e92fb17fdf652e873c44
Kerminate <kepeilin@outlook.com>
2017-11-29 22:26:48 +0800

    end origin

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -29,7 +29,7 @@
 </template>
 
 <script>
-import api from 'axios'
+import api from '@/axios'
 import Register from './Register'
 
 export default {
@@ -52,7 +52,55 @@ export default {
     }
   },
   methods: {
-    
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    },
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) { // 验证通过
+          let opt = this.loginForm
+          api.userLogin(opt)
+            .then(({ data }) => { // 解构赋值拿到data
+              // 账号不存在
+              if (data.info === false) {
+                this.$message({
+                  type: 'info',
+                  message: '账号不存在'
+                })
+                return false
+              }
+              // 账号存在
+              if (data.success) {
+                this.$message({
+                  type: 'success',
+                  message: '登录成功'
+                })
+                let token = data.token
+                let username = data.username
+                this.$store.dispatch('UserLogin', token)
+                this.$store.dispatch('UserName', username)
+                // 如果用户手动输入"/"那么会跳转到这里来，即this.$route.query.redirect有参数
+                let redirectUrl = decodeURIComponent(this.$route.query.redirect || '/')
+                // 跳转到指定的路由
+                this.$router.push({
+                  path: redirectUrl
+                })
+              } else {
+                this.$message({
+                  type: 'info',
+                  message: '密码错误！'
+                })
+              }
+            })
+        } else {
+          // 验证不通过
+          return false
+        }
+      })
+    }
+  },
+  components: {
+    Register
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,81 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import store from '../store/index'
 
 Vue.use(Router)
 
-export default new Router({
+// 路由懒加载 使用require.ensure来替代import,它能帮你将该组件以及该组件的所有依赖分割到一个单独的chunk中去
+const Login = resolve => {
+  // []用来指定该路由组件需要加载的依赖
+  require.ensure(['../components/Login.vue'], () => {
+    resolve(require('../components/Login.vue'))
+  })
+}
+
+const Register = resolve => {
+  require.ensure(['../components/Register.vue'], () => {
+    resolve(require('../components/Register.vue'))
+  })
+}
+
+const Hello = resolve => {
+  require.ensure(['../components/Hello.vue'], () => {
+    resolve(require('../components/Hello.vue'))
+  })
+}
+
+const Error404 = resolve => {
+  require.ensure(['../components/404.vue'], () => {
+    resolve(require('../components/404.vue'))
+  })
+}
+
+const router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Hello',
+      component: Hello,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '*',
+      name: 'error',
+      component: Error404
     }
   ]
 })
+
+// 注册全局钩子用来拦截导航
+router.beforeEach((to, from, next) => {
+  // 获取store里的token
+  let token = store.state.token
+  // 判断要去的路由有没有requiresAuth
+  if (to.meta.requiresAuth) {
+    if (token) {
+      next()
+    } else {
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath } // 将刚刚要去的路由path（却无权限）作为参数，方便登录成功后直接跳转到该路由
+      })
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
