00cc2a38edd9c4b2ecee0cfaa469aa59240f7680
Kerminate <kepeilin@outlook.com>
2017-11-30 19:01:39 +0800

    add koa

--- server/token/createToken.js
+++ server/token/createToken.js
@@ -1,39 +1,11 @@
 const jwt = require('jsonwebtoken')
 
-// 检查token是否过期
-module.exports = async (ctx, next) => {
-  // 拿到token
-  // console.log(ctx.request);的输出
-       // { method: 'GET',
-       // url: '/api/user',
-       // header:
-       // { 'accept-language': 'zh-CN,zh;q=0.8',
-       //     'accept-encoding': 'gzip, deflate, sdch, br',
-       //     referer: 'http://localhost:8000/',
-       //     authorization: 'token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTIzMTIzMTIzIiwiaWF0IjoxNDk0NDA1MDg4LCJleHAiOjE0OTQ0MDUwODh9.57iy3sL9TG0MTXBS7Xr6SS0QGRZObrivUloy-25NBqg',
-       //     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36',
-       //     accept: 'application/json, text/plain, */*',
-       //     connection: 'close',
-       //     host: 'localhost:8888' } }
-
-  if (ctx.request.header['authorization']) {
-    let token = ctx.request.header['authorization'].split(' ')[1]
-    // 解码token
-    let decoded = jwt.decode(token, 'kerminate')
-    // console.log(decoded);的输出 ：{ user_id: '123123123', iat: 1494405235, exp: 1494405235 }
-    if (token && decoded.exp <= new Date() / 1000) {
-      ctx.status = 401
-      ctx.body = {
-        message: 'token过期'
-      }
-    } else {
-      // 如果权限没问题，交给下一个控制器处理
-      return next()
-    }
-  } else {
-    ctx.status = 401
-    ctx.body = {
-      message: '没有token'
-    }
-  }
+// 登录时：核对用户名和密码成功后，应用将用户的id(图中的user_id)作为JWT Paylodad的一个属性
+module.exports = (userId) => {
+  const token = jwt.sign({
+    user_id: userId
+  }, 'kerminate', {
+    expiresIn: '60s' // 过期时间设置为60妙。那么decode这个token的时候得到的过期时间为 : 创建token的时间 + 设置的值
+  })
+  return token
 }
